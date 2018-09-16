e7e37a15ba03f8709355366c47403a310eca4e77
Oleg Krotenko <oleg.krotenko@sitecore.com>
2018-09-16 03:44:05 +0300

    Fix exception with automapper

--- code/LazyPythons.Sql/Mappers/CaffeExtensions.cs
+++ code/LazyPythons.Sql/Mappers/CaffeExtensions.cs
@@ -4,15 +4,15 @@ namespace LazyPythons.Sql.Mappers
 {
     public static class CaffeExtensions
     {
+
         public static LazyPythons.Models.Caffe ToApi(this LazyPythons.Sql.Data.Caffe obj)
         {
-            Mapper.Initialize(cfg=> cfg.CreateMap<LazyPythons.Sql.Data.Caffe, LazyPythons.Models.Caffe>());
             return Mapper.Map<LazyPythons.Sql.Data.Caffe, LazyPythons.Models.Caffe>(obj);
         }
 
         public static LazyPythons.Sql.Data.Caffe ToData(this LazyPythons.Models.Caffe obj)
         {
-            Mapper.Initialize(cfg => cfg.CreateMap<LazyPythons.Models.Caffe, LazyPythons.Sql.Data.Caffe>());
+
             return Mapper.Map<LazyPythons.Models.Caffe, LazyPythons.Sql.Data.Caffe>(obj);
         }
     }
--- code/LazyPythons.Sql/Mappers/MenuExtensions.cs
+++ code/LazyPythons.Sql/Mappers/MenuExtensions.cs
@@ -6,13 +6,11 @@ namespace LazyPythons.Sql.Mappers
     {
         public static LazyPythons.Models.Menu ToApi(this LazyPythons.Sql.Data.Menu obj)
         {
-            Mapper.Initialize(cfg => cfg.CreateMap<LazyPythons.Sql.Data.Menu, LazyPythons.Models.Menu>());
             return Mapper.Map<LazyPythons.Sql.Data.Menu, LazyPythons.Models.Menu>(obj);
         }
 
         public static LazyPythons.Sql.Data.Menu ToData(this LazyPythons.Models.Menu obj)
         {
-            Mapper.Initialize(cfg => cfg.CreateMap<LazyPythons.Models.Menu, LazyPythons.Sql.Data.Menu>());
             return Mapper.Map<LazyPythons.Models.Menu, LazyPythons.Sql.Data.Menu>(obj);
         }
     }
